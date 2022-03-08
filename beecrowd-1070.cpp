ecb2d362327f79f08834ec4644936dc261d0b08a
mahi1212 <mahi121.mr@gmail.com>
2022-03-08 20:01:15 +0600

    solved 2 problems

---
+++ beecrowd-1070.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if(n%2==0){
+        for(int i=n+1; i<= n+12; i+=2 ){
+            cout << i << endl;
+        }
+    }else{
+        for(int i=n; i<= n+11; i+=2 ){
+            cout << i << endl;
+        }
+    }
+
+
+    return 0;
+}
